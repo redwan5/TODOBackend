069f5c8ada2470e4ffa2981df881cedca67df56a
Redwan Khan <redwankhan534@gmail.com>
2025-03-15 03:16:02 +0600

    update

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require('express')
 const app = express();
 const path = require('path');
 const userModel = require('./models/user')
+require('dotenv').config();
 
 
+const PORT = process.env.PORT || 3000
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get ('/', (req, res) => {
     res.render("index");
--- models/user.js
+++ models/user.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect("mongodb://127.0.0.1:27017/testapp");
+// mongoose.connect("mongodb://127.0.0.1:27017/testapp");
+
+// mongoose.connect('mongodb+srv://redwankhan534:WSXmvmHcaGDfpuNc@cluster0.yugxy.mongodb.net/')
+const mongoURL = process.env.MONGODB_URL;
+
+mongoose.connect(mongoURL);
+// mongoose.connect('mongodb+srv://redwankhan534:WSXmvmHcaGDfpuNc@cluster0.yugxy.mongodb.net/')
 
 const userSchema = mongoose.Schema({
     image: String,
